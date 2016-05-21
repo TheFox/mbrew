ad331cc5b43cd2e126295cf998d5410300cd32ad
Christian Mayer <christian@fox21.at>
2016-05-21 10:28:19 +0200

    Ruby Common Big 2016-04-09

--- lib/mbrew/version.rb
+++ lib/mbrew/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module MBrew
 		RELEASE_ID = 3
-		VERSION = '0.1.2'
-		DATE = '2015-12-10'
+		VERSION = '0.1.3-dev'
+		DATE = '2016-05-21'
 		HOMEPAGE = 'https://github.com/TheFox/mbrew'
 	end
 end
