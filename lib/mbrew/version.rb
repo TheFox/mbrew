2e68ca1bd53e4c50723a55c67f2ca4b51dbe3012
Christian Mayer <christian@fox21.at>
2016-05-21 10:30:01 +0200

    Version 0.1.3.

--- lib/mbrew/version.rb
+++ lib/mbrew/version.rb
@@ -1,8 +1,8 @@
 
 module TheFox
 	module MBrew
-		RELEASE_ID = 3
-		VERSION = '0.1.3-dev'
+		RELEASE_ID = 4
+		VERSION = '0.1.3'
 		DATE = '2016-05-21'
 		HOMEPAGE = 'https://github.com/TheFox/mbrew'
 	end
