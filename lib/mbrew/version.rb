3d0a0b2a541967c1af51c3547d00309ba0809d0a
Christian Mayer <christian@fox21.at>
2016-10-15 23:59:35 +0200

    GitLab Deployment added. #1

--- lib/mbrew/version.rb
+++ lib/mbrew/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module MBrew
 		RELEASE_ID = 4
-		VERSION = '0.2.0-dev.2'
-		DATE = '2016-09-12'
+		VERSION = '0.2.0-dev.3'
+		DATE = '2016-10-16'
 		HOMEPAGE = 'https://github.com/TheFox/mbrew'
 	end
 end
