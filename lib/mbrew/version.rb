84a9ead606b915bd25cbbf6b3a2a931fc4aac187
Christian Mayer <christian@fox21.at>
2016-10-30 20:41:59 +0100

    Clean up.

--- lib/mbrew/version.rb
+++ lib/mbrew/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module MBrew
 		RELEASE_ID = 4
-		VERSION = '0.2.0-dev.3'
-		DATE = '2016-10-16'
+		VERSION = '0.2.0-dev.4'
+		DATE = '2016-10-30'
 		HOMEPAGE = 'https://github.com/TheFox/mbrew'
 	end
 end
